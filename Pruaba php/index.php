8aed08b7566430bfedc43669a79c3400dd31fa27
SebastianLL <71473385+SebastianLL@users.noreply.github.com>
2020-11-12 22:05:21 -0700

    Add files via upload

---
+++ Pruaba php/index.php
@@ -0,0 +1,22 @@
+<?php 
+
+	$nombre = 'Sebastian Lopez Leon';
+
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Site
+	</title>
+</head>
+<body>
+
+	<p>
+		hola que tal mi nombre es <?php echo $nombre; ?>
+	</p>
+
+</body>
+</html>
